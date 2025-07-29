cc496d0bcb3a62ff31c53d5a87ec5f4040054be8
Chinseok Lee <me@pyhub.kr>
2025-07-29 15:58:42 +0900

    크롤링 1단계 예시 추가

---
+++ cralwing/pyhub_melon.py
@@ -0,0 +1,13 @@
+import requests  # pip install requests
+from bs4 import BeautifulSoup  # pip install beautifulsoup4
+
+url = "https://pyhub.kr/melon/20231204.html"
+
+res = requests.get(url)
+html = res.text
+soup = BeautifulSoup(html, "html.parser")
+el_list = soup.select("#song-list li")
+for el in el_list:
+    song_title = el.select_one("h2").text
+    song_title = " ".join(song_title.split())
+    print(song_title)
